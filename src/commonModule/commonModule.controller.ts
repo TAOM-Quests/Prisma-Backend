c6bb0cd1ceaa745de4d4b083e9bdba96cb7ed209
Roman Nichipurenko <roman.nichi.o@gmail.com>
2025-04-16 17:19:07 +0400

    tq-21: Add uploadFile

--- src/commonModule/commonModule.controller.ts
+++ src/commonModule/commonModule.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query, StreamableFile } from "@nestjs/common";
+import { Controller, Get, Post, Query, StreamableFile, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { CommonModuleService } from "./commonModule.service";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @ApiTags('commonModule')
 @Controller('commonModule')
@@ -9,8 +10,16 @@ export class CommonModuleController {
     private commonModuleService: CommonModuleService
   ) {}
 
-  @Get('/file')
+  @Get('file')
   async getFile(@Query('fileName') fileName: string): Promise<StreamableFile> {
     return this.commonModuleService.getFile(fileName)
-  }  
+  }
+
+  @Post('file')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<string> {
+    console.log('FILE UPLOADED', file.originalname)
+
+    return file.filename
+  }
 }
--- src/commonModule/commonModule.module.ts
+++ src/commonModule/commonModule.module.ts
@@ -1,8 +1,21 @@
 import { Module } from "@nestjs/common";
 import { CommonModuleController } from "./commonModule.controller";
 import { CommonModuleService } from "./commonModule.service";
+import { MulterModule } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
 
 @Module({
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: `${process.cwd()}/public`,
+        filename: (_, file, cb) => {
+          const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+          cb(null, uniquePrefix + '.' + file.originalname.split('.').pop())
+        }
+      })
+    }),
+  ],
   controllers: [CommonModuleController],
   providers: [CommonModuleService],
 })
